4f7264d4850d15759416249393f00afd75757e8a
linhaobuaa <1257819385@qq.com>
2016-10-12 17:24:55 +0800

    lcr

--- turkey/news/views.py
+++ turkey/news/views.py
@@ -31,6 +31,9 @@ def infor_search():
 def interview_detail():
     return render_template('index/interview_detail.html')
 
+@mod.route('/person_detail/')
+def person_detail():
+    return render_template('index/card/person_detail.html')
 
 @mod.route('/turkey_info/')
 def turkey_info():
